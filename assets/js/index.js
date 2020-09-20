b133a0a5e3e6884e0d7f53a4733d5d0e394242e7
Nelson Alvarez <nalvare84523@universidadean.edu.co>
2020-09-19 22:32:48 -0500

    Actualizacion

---
+++ assets/js/index.js
@@ -0,0 +1,40 @@
+$('.toggle').click(function(){
+    $('.formulario').animate({
+        height:"toggle",
+        'padding-top': 'toggle',
+        'padding-bottom':'toggle',
+        opacity: 'toggle'
+    }, "slow");
+})
+
+
+document.getElementById('formTask').addEventListener('submit', guardarUsuario);
+
+function guardarUsuario(e){
+    let usuario = (document.getElementById('usuario').value);
+    let contraseña = (document.getElementById('contraseña').value);
+    let correo = (document.getElementById('correo').value);
+    let telefono = (document.getElementById('telefono').value);
+
+    const registro ={
+        usuario,
+        contraseña,
+        correo,
+        telefono
+};
+
+if (localStorage.getItem('registros') === null) {
+    let registros = [];
+    registros.push(registro);
+    localStorage.setItem('registros', JSON.stringify(registros));
+
+
+}else{
+    location.href = '/assets/pages/listaDeTareas.html';
+}
+
+e.preventDefault();
+
+
+
+}
